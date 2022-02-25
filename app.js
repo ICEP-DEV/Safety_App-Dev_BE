64e5b738bcac5b110af582d15c1c2e11718bcfd2
makofane41 <makofane411@gmail.com>
2022-02-25 09:48:38 +0200

    Updated

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = require('./routes/testimonial_api');
+const router = require('./routes/reportapi');
+
 const app = express();
 const port = process.env.PORT || 5001
 
@@ -17,4 +19,8 @@ app.use('/',router);
 //Display on console log
 //app.listen(port,()=> console.log('listen on port '+ port))
 app.listen(port,() => console.log(`Server running on port: http://localhost:${port}`));
+
+
+
+
 
