077e1462f9c4b42acbf892dc66942822ba0196ef
makofane41 <makofane411@gmail.com>
2022-02-25 08:40:54 +0200

    REPORT INCIDENT API

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./routes/reportapi');
+const app = express();
+const port = process.env.PORT || 5001
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//calling path
+app.use('/',router);
+
+
+//listing port
+app.listen(port,()=> console.log('listen on port '+ port))
