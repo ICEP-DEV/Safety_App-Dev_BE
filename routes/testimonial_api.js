2090acc8a889cdbcfb5e7b634723bdc2c429430a
Michael <94140658+Michael-Dlamini@users.noreply.github.com>
2022-03-09 13:57:12 +0200

    Update testimonial_api.js

--- routes/testimonial_api.js
+++ routes/testimonial_api.js
@@ -1,54 +1,61 @@
 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const Connection = require('mysql/lib/Connection');
-const routerTestimonialAPI= express.Router();
+const router = express.Router();
 
-
-//mySql connection 
 const pool = mysql.createPool({
     connectionLimit   :10,
-    host              : 'eu-cdbr-west-02.cleardb.net',
-    user              : 'b51544d9260432',
-    password          : 'dbd5ef6b',
-    database          : 'heroku_44548bd383ff010'
+    host              : 'localhost',
+    user              : 'root',
+    password          : '',
+    database          : 'GBV'
 
 })
 
-//Call
-routerTestimonialAPI.get('/v/', (req, res) => {
-    pool.getConnection((err, connection) => {
+router.get('/get/', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
         if(err) throw err
         console.log('connected as id ' + connection.threadId)
-        connection.query('SELECT user,testimonial_descr,testimonial_date from testimonial', (err, rows) => {
+        connection.query('SELECT * FROM testimonial', (err, rows) => 
+        {
             connection.release() // return the connection to pool
 
-            if (!err) {
+            if (!err) 
+            {
                 res.send(rows)
-            } else {
+            } else 
+            {
                 console.log(err)
             }
 
-            // optional
-            console.log('The data from reported Testimony table are: \n', rows)
+            
+            console.log('The data from Testimony table is : \n', rows)
         })
     })
-})
+} )
 
 
+router.post('/post/', (req, res) => 
+{
 
-routerTestimonialAPI.post('/r/', (req, res) => {
-
-    pool.getConnection((err, connection) => {
+    pool.getConnection((err, connection) => 
+    {
         if(err) throw err
         
         const params = req.body
-        connection.query('INSERT INTO testimonial SET ?', params, (err, rows) => {
+        connection.query('INSERT INTO testimonial SET ?', params, (err, rows) => 
+        {
         connection.release() 
-        if (!err) {
-            res.send(`Successfully submitted the tesimony` + params.name)
-        } else {
+        if (!err) 
+        {
+            res.send(`Successfully Added Testimony`)
+        } else 
+        {
             console.log(err)
         }
         
@@ -59,5 +66,5 @@ routerTestimonialAPI.post('/r/', (req, res) => {
     })
 });
 
-module.exports = routerTestimonialAPI;
+module.exports = router;
 
