150c47b60c24224bdded541fc8aac651af99acdb
Given Makofane <93738259+makofane41@users.noreply.github.com>
2022-04-26 09:59:13 +0200

    info-api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routerReportApi = require('./routes/reportapi');
 const routerTestimonialApi = require('./routes/testimonial_api');
 const routerVECApi = require('./routes/vecapi');
 const RouterHostpotAPI = require('./routes/hostpotIncidentAPI');
+const routerInfoCentreAPI = require('./routes/infocentre');
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.use('/',routerReportApi);
 app.use('/',routerTestimonialApi);
 app.use('/',routerVECApi);
 app.use('/',RouterHostpotAPI);
+app.use('/',routerInfoCentreAPI);
 
 
 //Display on console log
