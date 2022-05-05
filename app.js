10a137a904a481198000df634658359c9afb5353
ASZ000717 <andilesiyanda2000@gmail.com>
2022-05-05 12:03:32 +0200

    VEC_API

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routerReportApi = require('./routes/reportapi');
-const routerTestimonialApi = require('./routes/testimonial_api');
-const routerVECApi = require('./routes/vecapi');
-const RouterHostpotAPI = require('./routes/hostpotIncidentAPI');
-const routerInfoCentreAPI = require('./routes/infocentre');
-
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 5001
+
+const router = require('./routes/vec')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//calling path
-app.use('/',routerReportApi);
-app.use('/',routerTestimonialApi);
-app.use('/',routerVECApi);
-app.use('/',RouterHostpotAPI);
-app.use('/',routerInfoCentreAPI);
+app.use('/',router)
 
-
-//Display on console log
-
-app.listen(port,() => console.log(`Server running on port: http://localhost:${port}`));
+app.use(express.json())
 
 
 
+const port = process.env.PORT || 5001
 
-
+app.listen(port, '0.0.0.0', () =>
+{
+    console.log(`Server running on port: http://localhost:${port}`);
+})
+ 
---
+++ routes/vec.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+const mysql = require('mysql');
+const Connection = require('mysql/lib/Connection');
+const { request } = require('express');
+
+const pool = mysql.createPool({
+    connectionLimit   :  10,
+    host              : 'localhost',
+    user              : 'root',
+    password          : '',
+    database          : 'gbv'
+
+})
+
+router.get('/api/view/', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
+        if(err) throw err
+        //console.log('connected as id ' + connection.threadId)
+        connection.query('SELECT * FROM vec', (err, rows) => 
+        {
+            connection.release() // return the connection to pool
+
+            if (!err) 
+            {
+                res.send(rows)
+            }
+            else 
+            {
+                console.log(err)
+            }
+        })
+    })
+})
+
+router.get('/api/search/:vec_id', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
+        if(err) throw err
+       //console.log('connected as id ' + connection.threadId)
+        connection.query('SELECT * FROM vec WHERE vec_id = ?', [req.params.vec_id], (err, rows) => 
+        {
+            connection.release() // return the connection to pool
+
+            if (!err) 
+            {
+                res.send(rows)
+            } 
+            else 
+            {
+                console.log(err)
+            }
+        })
+    })
+})
+
+router.post('/api/post/', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
+        if(err) throw err
+        const params = req.body
+        connection.query('INSERT INTO vec SET ?', params, (err, rows) => 
+        {
+            connection.release() // return the connection to pool
+            if (!err) 
+            {
+            res.send(`VEC with Name : ${params.employee_name} has been added.`)
+            } 
+            else 
+            {
+            console.log(err)
+            }
+        })
+    })
+})
+
+router.put('/api/update', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
+        if(err) throw err
+        console.log('connected as id ' + connection.threadId)
+
+        const {vec_id, employee_name, contact_num, office_num, chat_id, email, surname, DOB} = req.body
+
+        connection.query('UPDATE vec SET employee_name = ?, contact_num = ?, office_num = ?, chat_id = ?, email = ?, surname = ?, DOB = ? WHERE vec_id = ?', [employee_name, contact_num, office_num, chat_id, email, surname, DOB, vec_id], (err, rows) => 
+        {
+            connection.release() // return the connection to pool
+
+            if (!err) 
+            {
+                res.send(`VEC with the name : ${employee_name} has been updated.`)
+            }
+            else 
+            {
+                console.log(err)
+            }
+        })
+    })
+})
+
+router.delete('/api/delete/:vec_id', (req, res) => 
+{
+    pool.getConnection((err, connection) => 
+    {
+        if(err) throw err
+        console.log('connected as id ' + connection.threadId)
+        connection.query('DELETE FROM vec WHERE vec_id = ?', [req.params.vec_id], (err, rows) => 
+        {
+            connection.release() // return the connection to pool
+
+            if (!err) 
+            {
+                res.send(`VEC with Record ID : ${[req.params.vec_id]} has been deleted.`)
+            }
+            else 
+            {
+                console.log(err)
+            }
+        })
+    })
+})
+
+module.exports = router;
